65967bb1c0de8910ea0610b332606155025516c3
swapnila95 <swapnilap95@gmail.com>
2019-11-17 14:04:53 +0530

    Initial commit

---
+++ django_project/users/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegistrationForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to login')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+# message.debug
+# message.info
+# message.success
+# message.warning
+# message.error
