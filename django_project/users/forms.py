1f34f0646bf7e3e9798558476f9ccc88aacaa7f8
swapnila95 <swapnilap95@gmail.com>
2019-11-18 23:57:57 +0530

    Basics of : login/login (submit)/ sign up now - done logout/ login again - done register - done profile - done

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
-class UserRegistrationForm(UserCreationForm):
+class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
